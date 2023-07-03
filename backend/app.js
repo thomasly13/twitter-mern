cb5741b4a22999fc3f7c3d25f603e02eceea0c39
Thomas Ly <Thomaslyy13@gmail.com>
2023-07-02 18:01:13 -0700

    added user model

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const usersRouter = require('./routes/api/users');
 const tweetRouter = require('./routes/api/tweets')
 const csrfRouter = require('./routes/api/csrf');
 
+
 const app = express();
 
 app.use(logger('dev'));
