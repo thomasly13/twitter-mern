2f2cebbaf41b0f3bee863be3d6d4a737d6f7eee0
Thomas Ly <Thomaslyy13@gmail.com>
2023-07-02 17:55:32 -0700

    add csrf protection

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
+
+const csurf = require('csurf');
+const cors = require('cors');
+const { isProduction } = require('./config/keys')
+
 
 const usersRouter = require('./routes/api/users');
 const tweetRouter = require('./routes/api/tweets')
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -12,7 +19,50 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+}
+
+
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
+
+  
+
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetRouter)
+app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+  
+  const serverErrorLogger = debug('backend:error');
+  
+  // Express custom error handler that will be called whenever a route handler or
+  // middleware throws an error or invokes the `next` function with a truthy value
+  app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
 
 module.exports = app;
