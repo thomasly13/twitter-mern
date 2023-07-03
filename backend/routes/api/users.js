a47732d2396e1dd909d570599268298b0e409deb
Thomas Ly <Thomaslyy13@gmail.com>
2023-07-03 00:39:13 -0700

    made some front end action creators

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -87,12 +87,35 @@ router.post('/login', validateLoginInput, async (req, res, next) => {
   })(req, res, next);
 });
 
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id)
+                             .populate("email");
+    return res.json(user);
+  }
+  catch(err) {
+    const error = new Error('user not found');
+    error.statusCode = 404;
+    error.errors = { message: "No user found with that id" };
+    return next(error);
+  }
+});
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({
-    message: "GET /api/users"
-  })
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find()
+                              .populate("email")
+                              .sort({ createdAt: -1 });
+    return res.json(users);
+  }
+  catch(err) {
+    return res.json([]);
+  }
 });
 
 module.exports = router;
+
+
+
